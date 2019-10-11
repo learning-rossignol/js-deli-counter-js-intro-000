8ec8cd012e1dc481cb2013c2ac2b283d4f8c32f9
Jean-François Rompré <jrompre@gmail.com>
2019-10-11 20:21:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var katzDeliLine = [];
 
 function takeANumber(line, name) {
 
-  return `Welcome, ${name}. You are number ${line.length} in line.`
+  return `Welcome, ${name}. You are number ${line.length + 1} in line.`
 }
