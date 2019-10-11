35032513b915ba3099c1b235bd0afa8090cce978
Jean-François Rompré <jrompre@gmail.com>
2019-10-11 20:36:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,24 @@ var katzDeliLine = [];
 function takeANumber(line, name) {
   return `Welcome, ${name}. You are number ${line.length + 1} in line.`
 }
+
+function nowServing(line) {
+  let next = line[0];
+  line.shift();
+  return next;
+}
+
+
+function currentLine( line){
+
+  let sfx = (line.length === 0) ? " empty" : ": " + (line.length - 1);
+  return
+
+  function enumLine() {
+    var store = "";
+    for (let i=0; i < line.length; i++) {
+      let pfx = ((store === "") ? "" : ", ");
+      store +=  `${pfx}${i + 1}. ${line[i]}`
+    }
+  }
+}
